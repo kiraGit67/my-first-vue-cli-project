deac03284b8d1eb82c23a29f8fcac74b0d247082
kiraGit67 <kirsten@sais.de>
2023-03-28 21:51:37 +0200

    imports new component BaseButton to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <br /><br />
+  <BaseButton />
+  <BaseButton text="Klick Mich" variant="secondary" />
+  <BaseButton text="Success!!!" variant="success" />
+  <BaseButton text="!!Attention!!" variant="warning" />
+  <BaseButton text="Error!!!" variant="error" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import BaseButton from "./components/BaseButton.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    BaseButton,
   },
 };
 </script>
