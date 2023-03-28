2d8ed7ddae3c7938d5107812ca3d938fb5cc61e6
kiraGit67 <kirsten@sais.de>
2023-03-28 21:51:02 +0200

    adds src/components/BaseButton.vue

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button :class="`btn-${variant}`">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "Click!",
+    },
+    variant: {
+      type: String,
+      default: "primary",
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  border: 1px solid transparent;
+  background-color: transparent;
+  padding: 0.5rem 0.75rem;
+  border-radius: 5px;
+  font-size: 1.75rem;
+  margin-right: 0.5rem;
+}
+
+.btn-primary {
+  background-color: dodgerblue;
+  border-color: dodgerblue;
+  color: white;
+}
+
+.btn-secondary {
+  background-color: lightgrey;
+  border-color: darkslategrey;
+  color: darkslategrey;
+}
+
+.btn-success {
+  background-color: yellowgreen;
+  border-color: darkgreen;
+  color: green;
+}
+
+.btn-warning {
+  background-color: #ffcc00;
+  border-color: #ffba00;
+  color: black;
+}
+
+.btn-error {
+  background-color: red;
+  border-color: darkred;
+  color: white;
+}
+</style>
