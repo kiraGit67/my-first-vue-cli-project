c9fa4891d34dfb6efeaac80456e8e94313b20ab6
kiraGit67 <kirsten@sais.de>
2023-04-03 18:18:18 +0200

    adds CarsView.vue

---
+++ src/views/CarsView.vue
@@ -0,0 +1,68 @@
+<template>
+  <cars-list
+    headline="Cars"
+    paragraph="Die besten Gebrauchtwagen"
+    :listItems="cars"
+  />
+</template>
+
+<script>
+import CarsList from "@/components/CarsList.vue";
+
+export default {
+  components: {
+    CarsList,
+  },
+  data() {
+    return {
+      cars: [
+        {
+          id: "001",
+          brand: "VW",
+          type: "Polo",
+          ps: 100,
+          km: 9000,
+          color: "silbergrau",
+          price: "€ 11.499",
+        },
+        {
+          id: "002",
+          brand: "VW",
+          type: "Golf",
+          ps: 115,
+          km: 7500,
+          color: "rauchblau",
+          price: "€ 13.899",
+        },
+        {
+          id: "003",
+          brand: "Audi",
+          type: "Quattro",
+          ps: 160,
+          km: 5750,
+          color: "schwarz",
+          price: "€ 28.699",
+        },
+        {
+          id: "004",
+          brand: "Renault",
+          type: "Modus",
+          ps: 110,
+          km: 6550,
+          color: "tizianrot",
+          price: "€ 12.699",
+        },
+        {
+          id: "014",
+          brand: "Renault",
+          type: "Twingo",
+          ps: 105,
+          km: 7250,
+          color: "silbergrau",
+          price: "€ 10.599",
+        },
+      ],
+    };
+  },
+};
+</script>
