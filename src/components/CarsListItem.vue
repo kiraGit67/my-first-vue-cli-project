8fcb96e31b6d1e2a3320ddb820151dd205e98b50
kiraGit67 <kirsten@sais.de>
2023-04-03 18:20:17 +0200

    adds CarsListItem component to import to CarsList component

---
+++ src/components/CarsListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <h1>
+      {{ brand }} <span>{{ type }}</span>
+    </h1>
+    <p>Kilometer-Stand: {{ km }} | {{ ps }} PS | {{ color }} | {{ price }}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    brand: String,
+    type: String,
+    ps: Number,
+    km: Number,
+    color: String,
+    price: String,
+  },
+};
+</script>
