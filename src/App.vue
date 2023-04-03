ad6c1683dc9ccccdd1b52739a4c18669ad22aba4
kiraGit67 <kirsten@sais.de>
2023-04-03 12:27:56 +0200

    overwrites the content in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <br /><br />
-  <BaseButton />
-  <BaseButton text="Klick Mich" variant="secondary" />
-  <BaseButton text="Success!!!" variant="success" />
-  <BaseButton text="!!Attention!!" variant="warning" />
-  <BaseButton text="Error!!!" variant="error" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import BaseButton from "./components/BaseButton.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    BaseButton,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -29,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
