82fcb8ce31ea263b7b4ec28fc584b348d2950851
kiraGit67 <kirsten@sais.de>
2023-04-03 12:29:18 +0200

    adds src/views/HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="@/assets/logo.png" />
+  <br /><br />
+  <BaseButton />
+  <BaseButton text="Klick Mich" variant="secondary" />
+  <BaseButton text="Success!!!" variant="success" />
+  <BaseButton text="!!Attention!!" variant="warning" />
+  <BaseButton text="Error!!!" variant="error" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import BaseButton from "@/components/BaseButton.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    BaseButton,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
