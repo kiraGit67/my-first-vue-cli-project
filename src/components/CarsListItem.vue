e818804628750364575aae5821907548c91ef75b
kiraGit67 <kirsten@sais.de>
2023-04-03 19:05:00 +0200

    integrates new car properties into CarsListItem component

--- src/components/CarsListItem.vue
+++ src/components/CarsListItem.vue
@@ -3,7 +3,14 @@
     <h1>
       {{ brand }} <span>{{ type }}</span>
     </h1>
-    <p>Kilometer-Stand: {{ km }} | {{ ps }} PS | {{ color }} | {{ price }}</p>
+    <p>
+      Baujahr {{ baujahr }} | Kilometer-Stand: {{ km }} | {{ ps }} PS | Preis
+      {{ price }}
+    </p>
+    <ul class="colors">
+      Erhältlich in den Farben
+      <li v-for="colour of colors" :key="colour">{{ colour }}</li>
+    </ul>
   </li>
 </template>
 
@@ -13,10 +20,45 @@ export default {
     id: String,
     brand: String,
     type: String,
+    baujahr: Number,
     ps: Number,
     km: Number,
     color: String,
+    colors: Array,
     price: String,
   },
 };
 </script>
+
+<style scoped>
+li {
+  padding-bottom: 0.75rem;
+  margin-bottom: 1.25rem;
+  border-bottom: 1px solid darkcyan;
+}
+
+li > h1 {
+  font-weight: normal;
+  color: #42cd78;
+}
+
+li > h1 > span {
+  font-size: 80%;
+  color: darkcyan;
+}
+
+ul.colors {
+  list-style-type: none;
+  padding-left: 0;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 1.25rem;
+}
+
+ul.colors li {
+  margin-bottom: 0;
+  padding-bottom: 0;
+  border-bottom: none;
+}
+</style>
