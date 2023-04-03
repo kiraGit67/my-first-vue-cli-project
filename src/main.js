8c8b65ceeb22e5c14e29ff6dddce6a42f0f036fd
kiraGit67 <kirsten@sais.de>
2023-04-03 12:29:53 +0200

    modifies src/main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import BaseButton from "@/components/BaseButton";
+import HelloWorld from "@/components/HelloWorld";
+import router from "./router";
+
+const app = createApp(App).use(router);
+app.component("BaseButton", BaseButton);
+app.component("HelloWorld", HelloWorld);
+app.mount("#app");
