ecf4ffd983a964a7d59767913cbd4f2c6d4f98d9
kiraGit67 <kirsten@sais.de>
2023-04-03 18:17:17 +0200

    adds BookList.vue - renames BaseList.vue in BookList.vue

---
+++ src/components/BookList.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ headline }}</h1>
+  <h2>{{ paragraph }}</h2>
+  <ul>
+    <book-list-item
+      v-for="listItem in listItems"
+      :key="listItem.isbn"
+      :cover="listItem.cover"
+      :title="listItem.title"
+      :subtitle="listItem.subtitle"
+      :abstract="listItem.abstract"
+      :author="listItem.author"
+      :publisher="listItem.publisher"
+      :isbn="listItem.isbn"
+      :numPages="listItem.numPages"
+    />
+  </ul>
+</template>
+
+<script>
+import BookListItem from "@/components/BookListItem.vue";
+
+export default {
+  components: { BookListItem },
+  name: "BaseList",
+  props: {
+    headline: String,
+    paragraph: String,
+    listItems: Array,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin: 0;
+}
+</style>
